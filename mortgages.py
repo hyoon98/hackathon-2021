575146a678da5169b84d4d54350a1c28d780e593
hya62 <hansoo_yoon@sfu.ca>
2021-11-16 19:40:29 -0800

    setup csv reading

---
+++ mortgages.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+cust=pd.read_csv("./data/CUST_DATASET_ST.csv")
+fsa=pd.read_csv("./data/FSA_DATASET_ST.csv")
+mg=pd.read_csv("./data/MG_DATASET_ST.csv")
+
+print(cust)
+print(fsa)
+print(mg)
